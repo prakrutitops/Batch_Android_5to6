e0a28f13771e5b0c47e14b7b42d81ba12ee15d21
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-06-12 17:55:42 +0530

    Add files via upload

---
+++ Module1/12-6-Introduction,setup/addition.kt
@@ -0,0 +1,30 @@
+fun main()
+{
+
+        //var-changable ,val-non changable
+
+        var a  = 5//Int
+        var b = 5.5//Float
+        val c= 'T'//Char
+        var d= 576.8678//Double
+        a=6
+
+
+
+        //c='A'
+        println(a)
+
+        println(b)
+
+        println(c)
+
+        println(d)
+
+
+        var a1 = 6
+        var a2 = 5
+
+        var sum = a1+a2
+        println(sum)
+
+}
---
+++ Module1/12-6-Introduction,setup/main.kt
@@ -0,0 +1,4 @@
+fun main()
+{
+    println("Hello From Tops")
+}
